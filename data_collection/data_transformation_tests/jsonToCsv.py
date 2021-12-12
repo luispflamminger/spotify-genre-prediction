c19e256e5ed8d1dee37aefe97da9c81ca52fff14
mkr992fom <90711597+mkr992fom@users.noreply.github.com>
2021-12-12 11:42:34 +0100

    jsonToCsv

---
+++ data_collection/data_transformation_tests/jsonToCsv.py
@@ -0,0 +1,27 @@
+import json
+import pandas as pd
+import os
+import csv
+
+f = open("/Users/martin/Documents/VSCode/BigData-Spotify/spotify-genre-prediction/data_collection/data_transformation_tests/result.json")
+resultJson = json.load(f)
+ 
+# now we will open a file for writing
+data_file = open('data_file.csv', 'w')
+# create the csv writer object
+csv_writer = csv.writer(data_file)
+
+count = 0
+ 
+for line in resultJson:
+    if count == 0:
+ 
+        # Writing headers of CSV file
+        header = line.keys()
+        csv_writer.writerow(header)
+        count += 1
+ 
+    # Writing data of CSV file
+    csv_writer.writerow(line.values())
+ 
+data_file.close()
